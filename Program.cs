b7c4a20c26ec7ece4080b66dbfc0be9086fd62f2
EzhVino_Office <valvic90@mail.ru>
2022-07-27 14:32:54 +0300

    3rd task completed

--- Program.cs
+++ Program.cs
@@ -17,22 +17,39 @@
 
 // Задать значения M и N Найти сумму натуральных элементов в промежутке от M до N
 
-int SumNumbers(int numFirst, int numSecond)
-{
-    if (numFirst == numSecond) return numSecond;
-    return (numFirst + SumNumbers(numFirst + 1, numSecond));
-}
+// int SumNumbers(int numFirst, int numSecond)
+// {
+//     if (numFirst == numSecond) return numSecond;
+//     return (numFirst + SumNumbers(numFirst + 1, numSecond));
+// }
 
-Console.Write("Enter M: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter N: ");
-int n = Convert.ToInt32(Console.ReadLine());
-if (n < m)
-    Console.WriteLine("Invalid range");
-else
-    Console.WriteLine(SumNumbers(m, n));
-
-
+// Console.Write("Enter M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n < m)
+//     Console.WriteLine("Invalid range");
+// else
+//     Console.WriteLine(SumNumbers(m, n));
 
 // Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+int Ackermann(int frstNum, int sndNum)
+{
+    if (frstNum == 0)
+        return sndNum + 1;
+    else if (frstNum > 0 && sndNum == 0)
+        return Ackermann(frstNum - 1, 1);
+    else if (frstNum > 0 && sndNum > 0)
+        return Ackermann(frstNum - 1, Ackermann(frstNum, sndNum - 1));
+    throw new Exception ("Incorrect arguments"); 
+}
+
+Console.Clear();
+Console.Write("Enter M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Result of the Ackermann function is {Ackermann(m, n)}");
