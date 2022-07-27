b6f70177405a6b33930c97f69061e2e576db9b20
EzhVino_Office <valvic90@mail.ru>
2022-07-27 12:42:39 +0300

    initial commit + 1st task

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N, M. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+Console.Write("Enter M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+if (n<m)
+    Console.WriteLine("Invalid range");
+else
+    Console.WriteLine(PrintNumbers(m, n));
+
+// Задать значения M и N Найти сумму натуральных элементов в промежутке от M до N
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
