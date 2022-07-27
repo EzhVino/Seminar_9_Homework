2b56c7a7727463df07c66fca639a72f1678f8e82
EzhVino_Office <valvic90@mail.ru>
2022-07-27 12:56:41 +0300

    2nd task completed

--- Program.cs
+++ Program.cs
@@ -1,21 +1,38 @@
 ﻿// Задайте значение N, M. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 
-string PrintNumbers(int start, int end)
+// string PrintNumbers(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start + 1, end));
+// }
+
+// Console.Write("Enter M: ");
+// int m = int.Parse(Console.ReadLine());
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine());
+// if (n < m)
+//     Console.WriteLine("Invalid range");
+// else
+//     Console.WriteLine(PrintNumbers(m, n));
+
+// Задать значения M и N Найти сумму натуральных элементов в промежутке от M до N
+
+int SumNumbers(int numFirst, int numSecond)
 {
-    if (start == end) return start.ToString();
-    return (start + " " + PrintNumbers(start + 1, end));
+    if (numFirst == numSecond) return numSecond;
+    return (numFirst + SumNumbers(numFirst + 1, numSecond));
 }
 
 Console.Write("Enter M: ");
-int m = int.Parse(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter N: ");
-int n = int.Parse(Console.ReadLine());
-if (n<m)
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < m)
     Console.WriteLine("Invalid range");
 else
-    Console.WriteLine(PrintNumbers(m, n));
+    Console.WriteLine(SumNumbers(m, n));
 
-// Задать значения M и N Найти сумму натуральных элементов в промежутке от M до N
+
 
 // Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
